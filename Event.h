0712c3820eed6f4d1d34b32efa8e5f9cd5106c37
qnfroadl <qnfroadl@gmail.com>
2025-04-16 07:00:58 +0900

    feat. EventManager 기능 구현
     - 우선순위 큐 사용
     - Event, EventType, EventData 클래스 추가
     - issue: Event객체 생성 ObjectPool사용하는걸로 변경 할 수 있음

---
+++ Event.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <string>
+
+enum class EventType;
+class EventData;
+class Event 
+{
+
+public:    
+    int priority;
+    EventType type;
+    EventData* data;
+    
+    // priority가 클수록 먼저 나와야 하니까 maxHeap 방식
+    bool operator > (const Event* other) const {
+        return priority > other->priority; // 작은 게 뒤로
+    }
+};
---
+++ EventData.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+
+class EventData
+{
+
+public:
+	EventData();
+	virtual ~EventData();
+
+};
