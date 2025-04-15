1932efb989b0d6a1c087629c3a2bc708ef514222
qnfroadl <qnfroadl@gmail.com>
2025-04-16 05:32:41 +0900

    feat. test. 카메라 기능 개발
     - 테스트용 플레이어도 추가
     - AstarScene에 적용
     - issue: 백버퍼 잔상 (게임에는 잔상문제 없게 하려면 여백을 크게 잡아둬야 할듯)

---
+++ Character.cpp
@@ -0,0 +1,33 @@
+﻿#include "Character.h"
+
+#include "KeyManager.h"
+#include "CommonFunction.h"
+#include "Camera.h"
+
+void Character::Update()
+{
+	if (KeyManager::GetInstance()->IsStayKeyDown('W'))
+	{
+		AddPos(0,-5);
+	}
+	else if(KeyManager::GetInstance()->IsStayKeyDown('A'))
+	{
+		AddPos(-5, 0);
+	}
+	else if (KeyManager::GetInstance()->IsStayKeyDown('S'))
+	{
+		AddPos(0, 5);
+	}
+	else if (KeyManager::GetInstance()->IsStayKeyDown('D'))
+	{
+		AddPos(5, 0);
+	}
+}
+
+void Character::Render(HDC hdc)
+{
+	float screenX = Camera::GetInstance()->GetPos().x;
+	float screenY = Camera::GetInstance()->GetPos().y;
+
+	RenderRectAtCenter(hdc, GetPos().x - screenX, GetPos().y - screenY, 50,50);
+}
