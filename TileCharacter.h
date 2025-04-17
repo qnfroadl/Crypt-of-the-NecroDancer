4205dda751e2b665077a8fa372eeea4a93aca64d
jinjucastle <guswlwl22@naver.com>
2025-04-17 12:39:52 +0900

    feat. TileCharacter 추가

---
+++ TileCharacter.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "TileActor.h"
+class Image;
+class TileMap;
+class PositionManager;
+class TileCharacter : public TileActor
+{
+
+protected:
+	string name;
+	POINT dir;
+	int gold;
+	Image* image;
+	vector<POINT> range;
+	float attack;
+
+	TileMap* tileMap;
+	PositionManager* positionManager;
+
+public:
+	inline string GetName() { return name; }
+	inline void SetName(string _name) { name = _name; }
+	inline POINT GetDir() { return dir; }
+	inline void SetDir(POINT _dir) { dir = _dir; }
+	inline int GetGold() { return gold; }
+	inline void SetGold(int _gold) { gold = _gold; }
+	inline float GetAttack() { return attack; }
+	inline void SetAttack(float _attack) { attack = _attack; }
+
+
+};
+
