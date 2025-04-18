84c141b27441111dc1bdffc9329adba9428d4989
chaseunggeun <tmdrms0711@gmail.com>
2025-04-18 14:14:27 +0900

    edit. TilemapTool

--- Block.cpp
+++ Block.cpp
@@ -1,1 +1,44 @@
 #include "Block.h"
+#include "Camera.h"
+#include "Image.h"
+#include "ImageManager.h"
+
+HRESULT Block::Init()
+{
+	blocklImage= ImageManager::GetInstance()->AddImage("WALL", TEXT("Image/Walls.bmp"), 216, 336, SAMPLE_WALL_X, SAMPLE_WALL_Y, true, RGB(255, 0, 255));
+	return S_OK;
+}
+
+void Block::Render(HDC hdc, FPOINT center)
+{
+	if (!blocklImage) return;
+
+	blocklImage->FrameRender(
+		hdc,
+		center.x,
+		center.y,
+		blockNum % SAMPLE_WALL_X,
+		blockNum / SAMPLE_WALL_X,
+		false, true // 중심 기준
+	);
+}
+
+bool Block::Destroy(Item* item)
+{
+    return false;
+}
+
+bool Block::Destroy(int strength)
+{
+	if (strength >= hp)
+	{
+		// TODO. 부수기
+	}
+    return false;
+}
+
+BlockType Block::GetTypeByBlockNum(int WallNum)
+{
+	//수정 예정
+	if (blockNum >= 0 && blockNum <= 15) return BlockType::NORMAL;
+}
--- Tile.cpp
+++ Tile.cpp
@@ -1,1 +1,78 @@
 #include "Tile.h"
+//#include "Trap.h"
+#include "Image.h"
+#include "Camera.h"
+#include "ImageManager.h"
+#include "Block.h"
+#include "Trap.h"
+
+HRESULT Tile::Init()
+{
+	tileImage = ImageManager::GetInstance()->AddImage("TILE", TEXT("Image/Tiles.bmp"), 234, 156, SAMPLE_TILE_X, SAMPLE_TILE_Y, true, RGB(255, 0, 255));
+	type = TileType::NONE;
+	tileNum = 0;
+	block = nullptr;
+	trap = nullptr;
+	light = 0.0f;
+//	rcTile = { 0, 0, 0, 0 };
+	return S_OK;
+}
+
+void Tile::Release()
+{
+	if (block)
+	{
+		delete block;
+		block = nullptr;
+	}
+	if (trap)
+	{
+		delete trap;
+		trap = nullptr;
+	}
+}
+
+void Tile::Render(HDC hdc, bool useCamera)
+{
+	if (!tileImage) return;
+
+	FPOINT center = {
+		rcTile.left + TILE_SIZE / 2.0f,
+		rcTile.top + TILE_SIZE / 2.0f
+	};
+
+	// 게임일 경우 카메라 좌표 보정
+	if (useCamera)
+	{
+		center.x -= Camera::GetInstance()->GetPos().x;
+		center.y -= Camera::GetInstance()->GetPos().y;
+	}
+
+	// 바닥
+	tileImage->FrameRender(hdc,
+		static_cast<int>(center.x), static_cast<int>(center.y),
+		tileNum % SAMPLE_TILE_X, tileNum / SAMPLE_TILE_X,
+		false, true);
+
+	//// 트랩
+	//if (trap)
+	//	trap->Render(hdc, center);
+
+	// 벽
+	if (block)
+		block->Render(hdc, center);
+}
+
+void Tile::OnTile(TileActor* actor)
+{
+	//if (trap)
+	//	trap->ontriggered(actor);
+}
+
+TileType Tile::GetTypeByTileNum(int tileNum)
+{
+	// 수정해야 함
+	if (tileNum >= 0 && tileNum <= 15) return TileType::NORMAL;
+	else if (tileNum >= 16 && tileNum <= 31) return TileType::NONE;
+	else return TileType::NONE;
+}
