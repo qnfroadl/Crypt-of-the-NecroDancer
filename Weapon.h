1e308477c2479f1240085b67e6d686b316041ebe
qnfroadl <qnfroadl@gmail.com>
2025-04-18 15:07:35 +0900

    feat. IPlayerObserver 추가
     - 단검,대검 리소스 추가
     - 플레이어 옵저버 기능 추가

---
+++ Weapon.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "TileActor.h"
+#include "WeaponData.h"
+
+class Monster;
+class Player;
+
+enum class DamageType
+{
+    NORMAL, PIERCING, PHASING,
+};
+
+// NORMAL, BLOOD, GLASS, GOLDEN, OBSIDIAN, TITANIUM
+class IMaterialProperty
+{
+public:
+	virtual int ModifyDamage(int baseDamage) = 0;
+	virtual void OnAttackMonster(Player* owner, Monster* monster) = 0;
+};
+
+// 대거, 브로드소드, 
+class IWeaponType
+{
+
+};
+
+
+
+class Weapon : TileActor
+{
+    DamageType damageType;
+    std::unique_ptr<IWeaponType> type;
+    std::unique_ptr<IMaterialProperty> material;
+
+public:
+    void SetMaterial(std::unique_ptr<IMaterialProperty> newMat) 
+    {
+        material = std::move(newMat);
+    }
+
+    void SetForm(std::unique_ptr<IWeaponType> newForm)
+    {
+        type = std::move(newForm);
+    }
+
+
+};
