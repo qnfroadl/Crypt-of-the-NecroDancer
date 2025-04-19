acbcc7875347fc98cf55c3a6949c53a6c6a34e47
chaseunggeun <tmdrms0711@gmail.com>
2025-04-19 09:37:28 +0900

    add. 타일맵 툴 타일맵 사이즈 지정 가능

---
+++ TilemapGenerator.h
@@ -0,0 +1,11 @@
+//#pragma once
+//#include "Singleton.h"
+//#include "Tilemap.h"
+//class TilemapGenerator : public Singleton<TilemapGenerator>
+//{
+//private:
+//
+//public:
+//	Tilemap* GenTilemap(..)
+//};
+//
