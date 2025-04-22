b2de61f6073ab6d75a180175679a7907f71faf80
qnfroadl <qnfroadl@gmail.com>
2025-04-23 01:49:26 +0900

    feat. y축 기반 정렬

--- TileActorRenderer.cpp
+++ TileActorRenderer.cpp
@@ -1,1 +1,50 @@
 ﻿#include "TileActorRenderer.h"
+#include "EventManager.h"
+
+void TileActorRenderer::AddRendableSource(std::shared_ptr<IRendableTileActor> source)
+{
+    rendableActors.push_back(source);
+}
+
+HRESULT TileActorRenderer::Init()
+{
+    
+
+
+    return S_OK;
+}
+
+void TileActorRenderer::Render(HDC hdc)
+{
+    std::vector<std::shared_ptr<TileActor>> yRender;
+
+    // 모든 IRendableTileActor에서 렌더 가능한 TileActor 수집
+    for (auto& source : rendableActors)
+    {
+        auto actors = source->GetRendableTileActors();
+        yRender.insert(yRender.end(), actors.begin(), actors.end());
+    }
+
+    // Y 좌표를 기준으로 정렬 (y가 작을수록 먼저 그리기)
+    std::sort(yRender.begin(), yRender.end(), [](const std::shared_ptr<TileActor>& a, const std::shared_ptr<TileActor>& b)
+        {
+            return a->GetPos().y < b->GetPos().y;       // t가 작은 값이 앞으로.
+        });
+
+    // 정렬된 순서대로 렌더링
+    for (auto& actor : yRender)
+    {
+        actor->Render(hdc);
+    }
+
+}
+
+void TileActorRenderer::OnPlayerMoved()
+{
+
+}
+
+void TileActorRenderer::OnTileDestroyed()
+{
+	
+}
--- TileActorRenderer.h
+++ TileActorRenderer.h
@@ -13,24 +13,32 @@
 	 - 라이팅 렌더링 (플레이어 이동 or 벽돌 파괴 이벤트를 받으면 라이팅 계산 할거고) 
 	     -> 카메라에 보이는 객체들중 라이팅 컴포넌트를 가진 객체를 찾는다.
 	     -> 라이팅 컴포넌트
+
+
 	- 매 프레임 y축 기반으로 렌더링 순서를 정렬.
 
 */
 
 class LightingRenderer;
-class TileActorRenderer
+class TileActorRenderer : public GameActor
 {
 
 private:
-	std::vector<std::shared_ptr<IRendableTileActor>> rendableSources;
-	std::vector<std::shared_ptr<TileActor>> CollectVisibleActors(const Camera& cam);
-
+	std::vector<std::shared_ptr<IRendableTileActor>> rendableActors;
 
 public:
+
+	HRESULT Init() override;
+	void Render(HDC hdc) override;
+
+	/*
+		PositionManager (PlayerManager, MonsterManager + Item까지 포함되어 있는 포지션 매니저)
+		Tilemap,
+
+
+		LightingRenderer에서 어떻게 할 지는 모르겠다.
+	*/
 	void AddRendableSource(std::shared_ptr<IRendableTileActor> source);
-
-	void Render(HDC hdc);
-
 	void OnPlayerMoved(); // 또는 OnLightingUpdateTrigger()
 	void OnTileDestroyed();
 
